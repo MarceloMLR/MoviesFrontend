b089bd214c7220860444e568305bfc9ec465439f
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2023-12-19 22:36:53 -0300

    Configurado theme e globalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './Styles/global'
+import theme from "./Styles/theme";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </ThemeProvider>
   </React.StrictMode>,
 )
